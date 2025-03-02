e52b951ec91ebbfd502ceff9b023131e3da3f878
jthu <jt.hu@mail.utoronto.ca>
2025-03-02 04:32:06 -0500

    add cleaning of temp files and subtitle (there is sth wrong with api calling at this point still working on it, perhaps an error from too large file size)

--- transcription_by_api.py
+++ transcription_by_api.py
@@ -1,11 +1,10 @@
 from dotenv import load_dotenv
 import os, json
-from pydub import AudioSegment
 from openai import OpenAI
 from pathlib import Path
 from openai.types.audio import Translation, TranslationVerbose, TranslationCreateResponse
 from utils.formatting import seconds_to_str_in_srt
-import subprocess
+import subprocess, shutil
 
 def extract_audio(filename: str):
     command = f"ffmpeg -i ./input/{filename} -ab 160k -ac 2 -ar 44100 -vn ./audio/{filename}/audio.wav"
@@ -34,7 +33,7 @@ def chunk_audio(filename: str) -> tuple[list[str], list[int]]:
     chunk the audio file at temp folder ./audio/filename/audio.wav into pieces, named as output_#.wav
     returns the number of pieces
     """
-    cmd = ['ffmpeg', '-i', f'./audio/{filename}/audio.wav', '-c', 'copy', '-map','0', '-segment_time', '00:01:00', '-f', 'segment', "-reset_timestamps", "1",  f'./audio/{filename}/output_%03d.wav']
+    cmd = ['ffmpeg', '-i', f'./audio/{filename}/audio.wav', '-c', 'copy', '-map','0', '-segment_time', '00:05:00', '-f', 'segment', "-reset_timestamps", "1",  f'./audio/{filename}/output_%03d.wav']
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     if result.returncode != 0:
         raise ValueError(f"Could not chunk the audio file error output:{result.stderr} output:{result.stdout}")
@@ -48,19 +47,23 @@ def chunk_audio(filename: str) -> tuple[list[str], list[int]]:
 
 
 DEBUG = True
+IF_FUSE = True
+FUSE_TYPE = 1 # whether to integrate the subtitle file into the video file
+FUSE_DICT = {1: "hardcode", 2: "track"}
 load_dotenv()  # take environment variables from .env.
 
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
 )
 
-if not os.path.isdir("./output"): # make sure output folder exists
-   os.makedirs("./output")
 
 if not os.path.isdir("./data"): # make sure output folder exists
    os.makedirs("./data")
 
 filename = input("file to operate to save the result as:")
+
+if not os.path.isdir(f"./output/{filename}/"): # make sure output folder exists
+   os.makedirs(f"./output/{filename}/")
 
 if not os.path.isfile(f"./input/{filename}"):
     raise FileNotFoundError("There is no such file in input folder, please double check")
@@ -70,40 +73,62 @@ if not os.path.isdir(f"./audio/{filename}"): # create a temp folder for processi
 
 
 path = f"./input/{filename}"
+try:
+    if filename.endswith("mp4") or filename.endswith("avi"):
+        extract_audio(filename)
 
-if filename.endswith("mp4") or filename.endswith("avi"):
-    extract_audio(filename)
+    original_audio_len = get_audio_length(filename, "audio.wav")
+    if original_audio_len / 60 > 10: # TODO: set the threshold to a reasonable value
+        filename_list, len_list = chunk_audio(filename)
+    else:
+        filename_list, len_list = ["audio.wav"], [original_audio_len]
 
-original_audio_len = get_audio_length(filename, "audio.wav")
-if original_audio_len / 60 > 10: # TODO: set the threshold to a reasonable value
-    filename_list, len_list = chunk_audio(filename) # TODO: chunk the audio file
-else:
-    filename_list, len_list = ["audio.wav"], [original_audio_len]
+    last_segment_ended_at = 0
+    index = 1
+    for i in range(len(filename_list)):
+        # do not process that is too short for openai whisper
+        if len_list[i] < 0.1:
+            continue
+        path = f"./audio/{filename}/{filename_list[i]}"
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=Path(path),
+            timestamp_granularities="segment",
+            response_format="verbose_json",
+        )
 
-last_segment_ended_at = 0
-index = 1
-for i in range(len(filename_list)):
-    path = f"./audio/{filename}/{filename_list[i]}"
-    transcription = client.audio.transcriptions.create(
-        model="whisper-1",
-        file=Path(path),
-        timestamp_granularities="segment",
-        response_format="verbose_json",
-    )
+        transcription_data = transcription.to_dict()
 
-    transcription_data = transcription.to_dict()
+        if DEBUG:
+            with open(f'./data/{filename}_data_{i}.json', 'w', encoding='utf-8') as f:
+                json.dump(transcription_data, f, ensure_ascii=False, indent=4)
 
-    if DEBUG:
-        with open(f'./data/{filename}_data_{i}.json', 'w', encoding='utf-8') as f:
-            json.dump(transcription_data, f, ensure_ascii=False, indent=4)
+        with open(f'./output/{filename}/{filename}.srt', 'a', encoding='utf-8') as f: # processing each chunked file
+            for line_segment in transcription_data["segments"]:
+                start = last_segment_ended_at + line_segment["start"]
+                end = last_segment_ended_at + line_segment["end"]
+                segment_text = line_segment["text"].strip()
+                start = seconds_to_str_in_srt(start)
+                end = seconds_to_str_in_srt(end)
+                f.write(f"{index} \n{start} --> {end} \n{segment_text}\n\n")
+                index += 1
+        last_segment_ended_at += len_list[i]
 
-    with open(f'./output/{filename}.srt', 'a', encoding='utf-8') as f: # processing each chunked file
-        for line_segment in transcription_data["segments"]:
-            start = last_segment_ended_at + line_segment["start"]
-            end = last_segment_ended_at + line_segment["end"]
-            segment_text = line_segment["text"].strip()
-            start = seconds_to_str_in_srt(start)
-            end = seconds_to_str_in_srt(end)
-            f.write(f"{index} \n{start} --> {end} \n{segment_text}\n\n")
-            index += 1
-    last_segment_ended_at += len_list[i]
+    if IF_FUSE:
+        # integrate subtitle file TODO
+        if FUSE_DICT[FUSE_TYPE] == "hardcode":
+            subprocess.run(f"ffmpeg -i ./input/{filename} -vf subtitles=./output/{filename}/{filename}.srt ./output/{filename}/{filename}", shell=True)
+        else:
+        # to hardcode the subtitle, use "ffmpeg -i ./input/sample_video.mp4 -vf subtitles=./output/sample_video.mp4.srt ./mysubtitledmovie.mp4"
+        # to put the subtitle in a track, use f"ffmpeg -i ./input/{filename} -i ./output/{filename}/{filename}.srt -c copy -c:s mov_text ./output/{filename}/{filename}""
+            subprocess.run(f"ffmpeg -i ./input/{filename} -i ./output/{filename}/{filename}.srt -c copy -c:s mov_text ./output/{filename}/{filename}", shell=True)
+
+    # TODO translation
+finally:
+    # clean the temp files
+    if os.path.isdir(f"./audio/{filename}"):
+        shutil.rmtree(f"./audio/{filename}")
+
+
+    # TODO, use local model
+    # TODO, a GUI
